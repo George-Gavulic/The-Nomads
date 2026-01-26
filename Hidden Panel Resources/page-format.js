45c568efb14718fd1f3a386df370369d84af82a7
George-Gavulic <georgegav522@gmail.com>
2026-01-26 00:18:23 -0500

    Added the start of file orginaization and added a css file

--- Hidden Panel Resources/page-format.js
+++ Hidden Panel Resources/page-format.js
@@ -2,4 +2,5 @@ document.getElementById("demo").addEventListener("click", myFunction);
 
 function myFunction() {
   document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+  document.getElementById("demo").classList.add("demo-suprise");
 }
