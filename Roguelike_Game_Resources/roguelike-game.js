82e6d4fa3ca353a97bcdd38bdd3513f0b4f258d4
c3r341 <garrett.ray98@gmail.com>
2026-02-02 21:03:45 -0500

    Box Moving Functionality
    The box now moves based on arrow key key press and key release.
    It will not leave the boarder of the canvas.

--- Roguelike_Game_Resources/roguelike-game.js
+++ Roguelike_Game_Resources/roguelike-game.js
@@ -1,8 +1,57 @@
+//class and context can be moved to input.js
+class InputHandler {
+    constructor(){
+        //Key press
+        document.addEventListener('keydown', (event) => {
+            //alert(event.keyCode); //check for keypress and give alert in browser with keycode
+            switch(event.keyCode){
+                case 37: //37 is the keycode for left arrow key
+                    character.moveLeft();
+                    break;
+                case 38: //38 is the keycode for up arrow key
+                    character.moveUp();
+                    break;
+                case 39: //39 is the keycode for right arrow key
+                    character.moveRight();
+                    break;
+                case 40: //40 is the keycode for down arrow key
+                    character.moveDown();
+                    break;
+            }
+        });
+        //Key release
+        document.addEventListener('keyup', (event) => {
+            switch(event.keyCode){
+                case 37: //37 is the keycode for left arrow key
+                    if(character.xspeed < 0)
+                        character.xstop();
+                    break;
+                case 38: //38 is the keycode for up arrow key
+                    if(character.yspeed < 0)
+                        character.ystop();
+                    break;
+                case 39: //39 is the keycode for right arrow key
+                    if(character.xspeed > 0)
+                        character.xstop();
+                    break;
+                case 40: //40 is the keycode for down arrow key
+                    if(character.yspeed > 0)
+                        character.ystop();
+                    break;
+            }
+        });
+    }
+}
+
 //class and context can be moved to character.js
 class Character {
     constructor(gameWidth, gameHeight){
         this.width = 150;
         this.height = 150;
+
+        this.maxSpeed = 7;
+        this.xspeed = 0;
+        this.yspeed = 0;
 
         this.position = {
             x: gameWidth / 2 - this.width / 2,
@@ -11,9 +60,43 @@ class Character {
 
     }
 
+    moveLeft(){
+        this.xspeed = -this.maxSpeed;
+    }
+    moveRight(){
+        this.xspeed = this.maxSpeed;
+    }
+    moveUp(){
+        this.yspeed = -this.maxSpeed;
+    }
+    moveDown(){
+        this.yspeed = this.maxSpeed;
+    }
+    xstop(){
+        this.xspeed = 0;
+    }
+    ystop(){
+        this.yspeed = 0;
+    }
+
     draw(ctx){
         ctx.fillStyle = '#0ff'
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update(deltaTime){
+        if(!deltaTime) return;
+
+        this.position.x += this.xspeed;
+        this.position.y += this.yspeed;
+
+        //keeps square within bounds of canvas
+        if(this.position.x < 0) this.position.x = 0;
+        if(this.position.x + this.width > GAME_WIDTH) this.position.x = GAME_WIDTH - this.width;
+
+        if(this.position.y < 0) this.position.y = 0;
+        if(this.position.y + this.height > GAME_HEIGHT) this.position.y = GAME_HEIGHT - this.height;
+
     }
 }
 
@@ -28,9 +111,11 @@ let character = new Character(GAME_WIDTH, GAME_HEIGHT);
 
 character.draw(ctx);
 
+new InputHandler();
+
 let lastTime = 0;
 
-
+//gameloop that updates every frame to redraw the canvas and player
 function gameLoop(timestamp){
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
@@ -40,12 +125,6 @@ function gameLoop(timestamp){
     character.draw(ctx);
 
     requestAnimationFrame(gameLoop);
-}
-//function should be moved to character.js
-update(deltaTime);{
-    if(!deltaTime) return;
-
-    this.position.x += 5 / deltaTime;
 }
     
 gameLoop();
