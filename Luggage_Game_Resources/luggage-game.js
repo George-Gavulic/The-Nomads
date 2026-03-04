98f4d8ea929862d6bbac229d72915e7fcee456ce
c3r341 <garrett.ray98@gmail.com>
2026-03-04 17:07:31 -0500

    Added new objects from the spritesheet
    Added 15 new objects from the furniture1 spritesheet of various sizes.
    
    Added the furniture2 spritesheet to the root
    
    Changed naming convention of objects to increase comprehension

--- Luggage_Game_Resources/luggage-game.js
+++ Luggage_Game_Resources/luggage-game.js
@@ -16,7 +16,8 @@ canvas.style.height = canvas.height * SCALE + "px";
 const TILESETS = {
     airport: "/AirPort.png",
     cracks: "/decorative_cracks_walls.png",
-    f1: "/Furniture1.png", 
+    f1: "/Furniture1.png",
+    f2: "/Furniture2.png", 
     gate: "/Door_1.png"
 };
 
@@ -29,6 +30,7 @@ const tileSheet = new Image();
 
 // TILE DEFINITIONS
 const tiles = {
+    999:{ name:"brownGate", solid:true, tileset:"gate", x:0, y:0 }, // <<< only one in use, all other are temp
     0: { name: "topLeftCorner", solid: false, tileset: "airport", x: 0, y: 6 },
     1: { name: "bottomLeftCorner", solid: false, tileset: "airport", x: 0, y: 7 },
     2: { name: "topRightCorner", solid: true,  tileset: "airport", x: 1, y: 6 },
@@ -47,48 +49,123 @@ const tiles = {
     15:{ name:"bottomRoadLeft", solid:true, tileset:"airport", x:5, y:4 },
     16:{ name:"bottomRoadRight", solid:true, tileset:"airport", x:6, y:4 },
 
-    17:{ name:"TopLeftTable", solid:true, tileset:"f1", x:0, y:0 },
-    18:{ name:"TopRightTable", solid:true, tileset:"f1", x:1, y:0 },
-    19:{ name:"BottomLeftTable", solid:true, tileset:"f1", x:0, y:1 },
-    20:{ name:"BottomRightTable", solid:true, tileset:"f1", x:1, y:1 },
+    //Size-(Orientation)-Color-Piece-Object
+    17:{ name:"SmallOrangeTopLeftEndtable", solid:true, tileset:"f1", x:0, y:0 },
+    18:{ name:"SmallOrangeTopRightEndtable", solid:true, tileset:"f1", x:1, y:0 },
+    19:{ name:"SmallOrangeBottomLeftEndtable", solid:true, tileset:"f1", x:0, y:1 },
+    20:{ name:"SmallOrangeBottomRightEndtable", solid:true, tileset:"f1", x:1, y:1 },
 
-    21:{ name:"LTopLeftTable", solid:true, tileset:"f1", x:0, y:2 },
-    22:{ name:"LTopMidTable", solid:true, tileset:"f1", x:1, y:2 },
-    23:{ name:"LTopRightTable", solid:true, tileset:"f1", x:2, y:2 },
-    24:{ name:"LBottomLeftTable", solid:true, tileset:"f1", x:0, y:3 },
-    25:{ name:"LBottomMidTable", solid:true, tileset:"f1", x:1, y:3 },
-    26:{ name:"LBottomRightTable", solid:true, tileset:"f1", x:2, y:3 },
+    21:{ name:"LargeTanTopLeftTable", solid:true, tileset:"f1", x:0, y:2 },
+    22:{ name:"LargeTanTopMidTable", solid:true, tileset:"f1", x:1, y:2 },
+    23:{ name:"LargeTanTopRightTable", solid:true, tileset:"f1", x:2, y:2 },
+    24:{ name:"LargeTanBottomLeftTable", solid:true, tileset:"f1", x:0, y:3 },
+    25:{ name:"LargeTanBottomMidTable", solid:true, tileset:"f1", x:1, y:3 },
+    26:{ name:"LargeTanBottomRightTable", solid:true, tileset:"f1", x:2, y:3 },
 
-    27:{ name:"LVTopLeftTable", solid:true, tileset:"f1", x:0, y:4 },
-    28:{ name:"LVLeftMidTable", solid:true, tileset:"f1", x:0, y:5 },
-    29:{ name:"LVTopRightTable", solid:true, tileset:"f1", x:0, y:6 },
-    30:{ name:"LVBottomLeftTable", solid:true, tileset:"f1", x:1, y:4 },
-    31:{ name:"LVLeftMidTable", solid:true, tileset:"f1", x:1, y:5 },
-    32:{ name:"LVBottomRightTable", solid:true, tileset:"f1", x:1, y:6 },
+    27:{ name:"LargeVertTanTopLeftTable", solid:true, tileset:"f1", x:0, y:4 },
+    28:{ name:"LargeVertTanLeftMidTable", solid:true, tileset:"f1", x:0, y:5 },
+    29:{ name:"LargeVertTanTopRightTable", solid:true, tileset:"f1", x:0, y:6 },
+    30:{ name:"LargeVertTanBottomLeftTable", solid:true, tileset:"f1", x:1, y:4 },
+    31:{ name:"LargeVertTanLeftMidTable", solid:true, tileset:"f1", x:1, y:5 },
+    32:{ name:"LargeVertTanBottomRightTable", solid:true, tileset:"f1", x:1, y:6 },
 
-    33:{ name:"B_TopLeftEndtable", solid:true, tileset:"f1", x:0, y:7 },
-    34:{ name:"B_LeftMidEndtable", solid:true, tileset:"f1", x:0, y:8 },
-    35:{ name:"B_TopRightEndtable", solid:true, tileset:"f1", x:0, y:9 },
-    36:{ name:"B_BottomLeftEndtable", solid:true, tileset:"f1", x:1, y:7 },
-    37:{ name:"B_RightMidEndtable", solid:true, tileset:"f1", x:1, y:8 },
-    38:{ name:"B_BottomRightEndtable", solid:true, tileset:"f1", x:1, y:9 },
+    33:{ name:"TallBrownLargeTopLeftEndtable", solid:true, tileset:"f1", x:0, y:7 },
+    34:{ name:"TallBrownLeftMidEndtable", solid:true, tileset:"f1", x:0, y:8 },
+    35:{ name:"TallBrownTopRightEndtable", solid:true, tileset:"f1", x:0, y:9 },
+    36:{ name:"TallBrownBottomLeftEndtable", solid:true, tileset:"f1", x:1, y:7 },
+    37:{ name:"TallBrownRightMidEndtable", solid:true, tileset:"f1", x:1, y:8 },
+    38:{ name:"TallBrownBottomRightEndtable", solid:true, tileset:"f1", x:1, y:9 },
 
-    39:{ name:"redGate", solid:true, tileset:"gate", x:0, y:0 },
-    40:{ name:"orangeGate", solid:true, tileset:"gate", x:1, y:0 },//some of these will become like a table gate or something.
-    41:{ name:"yellowGate", solid:true, tileset:"gate", x:2, y:0 },
-    42:{ name:"greenGate", solid:true, tileset:"gate", x:3, y:0 },
-    43:{ name:"blueGate", solid:true, tileset:"gate", x:4, y:0 },
-    44:{ name:"purpleGate", solid:true, tileset:"gate", x:5, y:0 },
-    45:{ name:"brownGate", solid:true, tileset:"gate", x:0, y:0 }, // <<< only one in use, all other are temp
-    46:{ name:"blackGate", solid:true, tileset:"gate", x:7, y:0 },
-    47:{ name:"grayGate", solid:true, tileset:"gate", x:8, y:0 },
-    48:{ name:"temp1Gate", solid:true, tileset:"gate", x:9, y:0 },
-    49:{ name:"temp2Gate", solid:true, tileset:"gate", x:10, y:0 },
-    50:{ name:"temp3Gate", solid:true, tileset:"gate", x:11, y:0 },
-    51:{ name:"temp4Gate", solid:true, tileset:"gate", x:12, y:0 },
-    52:{ name:"temp5Gate", solid:true, tileset:"gate", x:13, y:0 },
-    53:{ name:"temp6Gate", solid:true, tileset:"gate", x:14, y:0 },
-    54:{ name:"temp7Gate", solid:true, tileset:"gate", x:15, y:0 },
+    39:{ name:"SmallOrangeSideTopEndtable", solid:true, tileset:"f1", x:2, y:0 },
+    40:{ name:"SmallOrangeSideBottomEndtable", solid:true, tileset:"f1", x:2, y:1 },
+
+    41:{ name:"SmallBrownTopEndtable", solid:true, tileset:"f1", x:3, y:0 },
+    42:{ name:"SmallBrownBottomEndtable", solid:true, tileset:"f1", x:3, y:1 },
+
+    43:{ name:"WhiteTopChair", solid:true, tileset:"f1", x:5, y:0 },
+    44:{ name:"WhiteBottomChair", solid:true, tileset:"f1", x:5, y:1 },
+
+    45:{ name:"WhiteSideTopChair", solid:true, tileset:"f1", x:6, y:0 },
+    46:{ name:"WhiteSideBottomChair", solid:true, tileset:"f1", x:6, y:1 },
+
+    47:{ name:"WhiteBackTopChair", solid:true, tileset:"f1", x:7, y:0 },
+    48:{ name:"WhiteBackBottomChair", solid:true, tileset:"f1", x:7, y:1 },
+
+    49:{ name:"TanTopChair", solid:true, tileset:"f1", x:8, y:0 },
+    50:{ name:"TanBottomChair", solid:true, tileset:"f1", x:8, y:1 },
+
+    51:{ name:"TanSideTopChair", solid:true, tileset:"f1", x:9, y:0 },
+    52:{ name:"TanSideBottomChair", solid:true, tileset:"f1", x:9, y:1 },
+
+    53:{ name:"TanBackTopChair", solid:true, tileset:"f1", x:10, y:0 },
+    54:{ name:"TanBackBottomChair", solid:true, tileset:"f1", x:10, y:1 },
+
+    55:{ name:"BrownTopStool", solid:true, tileset:"f1", x:12, y:1 },
+
+    56:{ name:"SmallTanTopLeftBookcase", solid:true, tileset:"f1", x:3, y:2 },
+    57:{ name:"SmallTanTopRightBookcase", solid:true, tileset:"f1", x:4, y:2 },
+    58:{ name:"SmallTanBottomLeftBookcase", solid:true, tileset:"f1", x:3, y:3 },
+    59:{ name:"SmallTanBottomRightBookcase", solid:true, tileset:"f1", x:4, y:3 },
+    
+    60:{ name:"LargeTanTopLeftBookcase", solid:true, tileset:"f1", x:2, y:4 },
+    61:{ name:"LargeTanTopMidBookcase", solid:true, tileset:"f1", x:3, y:4 },
+    62:{ name:"LargeTanTopRightBookcase", solid:true, tileset:"f1", x:4, y:4 },
+    63:{ name:"LargeTanMiddleLeftBookcase", solid:true, tileset:"f1", x:2, y:5 },
+    64:{ name:"LargeTanMiddleBookcase", solid:true, tileset:"f1", x:3, y:5 },
+    65:{ name:"LargeTanMiddleRightBookcase", solid:true, tileset:"f1", x:4, y:5 },
+    66:{ name:"LargeTanBottomLeftBookcase", solid:true, tileset:"f1", x:2, y:6 },
+    67:{ name:"LargeTanBottomMidBookcase", solid:true, tileset:"f1", x:3, y:6 },
+    68:{ name:"LargeTanBottomRightBookcase", solid:true, tileset:"f1", x:4, y:6 },
+
+    69:{ name:"LargeOrangeTopLeftBookcase", solid:true, tileset:"f1", x:6, y:4 },
+    70:{ name:"LargeOrangeTopMidBookcase", solid:true, tileset:"f1", x:7, y:4 },
+    71:{ name:"LargeOrangeTopRightBookcase", solid:true, tileset:"f1", x:8, y:4 },
+    72:{ name:"LargeOrangeMiddleLeftBookcase", solid:true, tileset:"f1", x:6, y:5 },
+    73:{ name:"LargeOrangeMiddleBookcase", solid:true, tileset:"f1", x:7, y:5 },
+    74:{ name:"LargeOrangeMiddleRightBookcase", solid:true, tileset:"f1", x:8, y:5 },
+    75:{ name:"LargeOrangeBottomLeftBookcase", solid:true, tileset:"f1", x:6, y:6 },
+    76:{ name:"LargeOrangeBottomMidBookcase", solid:true, tileset:"f1", x:7, y:6 },
+    77:{ name:"LargeOrangeBottomRightBookcase", solid:true, tileset:"f1", x:8, y:6 },
+
+    78:{ name:"TopLamp", solid:true, tileset:"f1", x:6, y:7 },
+    79:{ name:"MiddleLamp", solid:true, tileset:"f1", x:6, y:8 },
+    80:{ name:"BottomLamp", solid:true, tileset:"f1", x:6, y:9 },
+
+    81:{ name:"TanTopHatstand", solid:true, tileset:"f1", x:3, y:7 },
+    82:{ name:"TanMiddleHatstand", solid:true, tileset:"f1", x:3, y:8 },
+    83:{ name:"TanBottomHatstand", solid:true, tileset:"f1", x:3, y:9 },
+
+    84:{ name:"OrangeTopLeftClock", solid:true, tileset:"f1", x:4, y:7 },
+    85:{ name:"OrangeBrownLeftMidClock", solid:true, tileset:"f1", x:4, y:8 },
+    86:{ name:"OrangeTopRightClock", solid:true, tileset:"f1", x:4, y:9 },
+    87:{ name:"OrangeBottomLeftClock", solid:true, tileset:"f1", x:5, y:7 },
+    88:{ name:"OrangeRightMidClock", solid:true, tileset:"f1", x:5, y:8 },
+    89:{ name:"OrangeBottomRightClock", solid:true, tileset:"f1", x:5, y:9 },
+
+    90:{ name:"OrangeTopLeftCouch", solid:true, tileset:"f1", x:5, y:10 },
+    91:{ name:"OrangeTopLMidCouch", solid:true, tileset:"f1", x:6, y:10 },
+    92:{ name:"OrangeTopRMidCouch", solid:true, tileset:"f1", x:7, y:10 },
+    93:{ name:"OrangeTopRightCouch", solid:true, tileset:"f1", x:8, y:10 },
+    94:{ name:"OrangeBottomLeftCouch", solid:true, tileset:"f1", x:5, y:11 },
+    95:{ name:"OrangeBottomLMidCouch", solid:true, tileset:"f1", x:6, y:11 },
+    96:{ name:"OrangeBottomRMidCouch", solid:true, tileset:"f1", x:7, y:11 },
+    97:{ name:"OrangeBottomRightCouch", solid:true, tileset:"f1", x:8, y:11 },
+
+    //39:{ name:"redGate", solid:true, tileset:"gate", x:0, y:0 },
+    //40:{ name:"orangeGate", solid:true, tileset:"gate", x:1, y:0 },//some of these will become like a table gate or something.
+    //41:{ name:"yellowGate", solid:true, tileset:"gate", x:2, y:0 },
+    //42:{ name:"greenGate", solid:true, tileset:"gate", x:3, y:0 },
+    //43:{ name:"blueGate", solid:true, tileset:"gate", x:4, y:0 },
+    //44:{ name:"purpleGate", solid:true, tileset:"gate", x:5, y:0 },
+    //46:{ name:"blackGate", solid:true, tileset:"gate", x:7, y:0 },
+    //47:{ name:"grayGate", solid:true, tileset:"gate", x:8, y:0 },
+    //48:{ name:"temp1Gate", solid:true, tileset:"gate", x:9, y:0 },
+    //49:{ name:"temp2Gate", solid:true, tileset:"gate", x:10, y:0 },
+    //50:{ name:"temp3Gate", solid:true, tileset:"gate", x:11, y:0 },
+    //51:{ name:"temp4Gate", solid:true, tileset:"gate", x:12, y:0 },
+    //52:{ name:"temp5Gate", solid:true, tileset:"gate", x:13, y:0 },
+    //53:{ name:"temp6Gate", solid:true, tileset:"gate", x:14, y:0 },
+    //54:{ name:"temp7Gate", solid:true, tileset:"gate", x:15, y:0 },
 
     
 
@@ -120,7 +197,7 @@ const levels = {
     //lets try a 20 by 10 map for the demo, we can always add more levels later, but this is a good start for testing
     level1: {
         map:    [
-            [0,7,7,7,7,7,7,7,45,45,7,7,7,7,7,7,7,7,7,2],
+            [0,7,7,7,7,7,7,7,999,999,7,7,7,7,7,7,7,7,7,2],
             [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
             [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
             [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
@@ -132,8 +209,8 @@ const levels = {
             [15,16,15,16,15,16,15,16,15,16,15,16,15,16,15,16,15,16,15,16],
         ],
         blocks: [
-            { shape: "small_table", x: 8, y: 2, goals: [{x: 8, y: 1}] },
-            { shape: "large_table", x: 7, y: 4, goals: [{x: 8, y: 1}] }
+            { shape: "orange_couch", x: 8, y: 2, goals: [{x: 8, y: 1}] },
+            { shape: "large_tan_table", x: 7, y: 4, goals: [{x: 8, y: 1}] }
         ]
     },
     level2: {
@@ -141,8 +218,8 @@ const levels = {
             [0,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,2],
             [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
             [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
-            [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,45],
-            [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,45],
+            [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,999],
+            [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,999],
             [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
             [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
             [4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6],
@@ -150,8 +227,8 @@ const levels = {
             [15,16,15,16,15,16,15,16,15,16,15,16,15,16,15,16,15,16,15,16],
         ],
         blocks: [
-            { shape: "large_table_vert", x: 4, y: 2 },
-            { shape: "brown_endtable", x: 2, y: 3 }
+            { shape: "large_tan_table_vert", x: 4, y: 2 },
+            { shape: "large_brown_endtable", x: 2, y: 3 }
         ]
     }
 };
@@ -202,13 +279,18 @@ const SHAPES = {
         { x: 0, y: -2, tileId: 13 },
         { x: 1, y: 0, tileId: 14 }
     ],
-    small_table: [
+    //size-color-object-orientation
+    small_orange_endtable: [
         { x: 0, y: 0, tileId: 17 },
         { x: 1, y: 0, tileId: 18 },
         { x: 0, y: 1, tileId: 19 },
         { x: 1, y: 1, tileId: 20 }
     ],
-    large_table: [
+    small_orange_endtable_side: [
+        { x: 0, y: 0, tileId: 39 },
+        { x: 0, y: 1, tileId: 40 },
+    ],
+    large_tan_table: [
         { x: 0, y: 0, tileId: 21 },
         { x: 1, y: 0, tileId: 22 },
         { x: 2, y: 0, tileId: 23 },
@@ -216,7 +298,7 @@ const SHAPES = {
         { x: 1, y: 1, tileId: 25 },
         { x: 2, y: 1, tileId: 26 }
     ],
-    large_table_vert: [
+    large_tan_table_vert: [
         { x: 0, y: 0, tileId: 27 },
         { x: 0, y: 1, tileId: 28 },
         { x: 0, y: 2, tileId: 29 },
@@ -224,14 +306,101 @@ const SHAPES = {
         { x: 1, y: 1, tileId: 31 },
         { x: 1, y: 2, tileId: 32 }
     ],
-    brown_endtable: [
+    large_brown_endtable: [
         { x: 0, y: 0, tileId: 33 },
         { x: 0, y: 1, tileId: 34 },
         { x: 0, y: 2, tileId: 35 },
         { x: 1, y: 0, tileId: 36 },
         { x: 1, y: 1, tileId: 37 },
         { x: 1, y: 2, tileId: 38 }
-    ]
+    ],
+    small_brown_endtable: [
+        { x: 0, y: 0, tileId: 41 },
+        { x: 0, y: 1, tileId: 42 },
+    ],
+    white_chair: [
+        { x: 0, y: 0, tileId: 43 },
+        { x: 0, y: 1, tileId: 44 },
+    ],
+    white_side_chair: [
+        { x: 0, y: 0, tileId: 45 },
+        { x: 0, y: 1, tileId: 46 },
+    ],
+    white_back_chair: [
+        { x: 0, y: 0, tileId: 47 },
+        { x: 0, y: 1, tileId: 48 },
+    ],
+    tan_chair: [
+        { x: 0, y: 0, tileId: 49 },
+        { x: 0, y: 1, tileId: 50 },
+    ],
+    tan_side_chair: [
+        { x: 0, y: 0, tileId: 51 },
+        { x: 0, y: 1, tileId: 52 },
+    ],
+    tan_back_chair: [
+        { x: 0, y: 0, tileId: 53 },
+        { x: 0, y: 1, tileId: 54 },
+    ],
+    brown_stool: [
+        { x: 0, y: 0, tileId: 55 },
+    ],
+    small_tan_bookcase: [
+        { x: 0, y: 0, tileId: 56 },
+        { x: 1, y: 0, tileId: 57 },
+        { x: 0, y: 1, tileId: 58 },
+        { x: 1, y: 1, tileId: 59 },
+    ],
+    large_tan_bookcase: [
+        { x: 0, y: 0, tileId: 60 },
+        { x: 1, y: 0, tileId: 61 },
+        { x: 2, y: 0, tileId: 62 },
+        { x: 0, y: 1, tileId: 63 },
+        { x: 1, y: 1, tileId: 64 },
+        { x: 2, y: 1, tileId: 65 },
+        { x: 0, y: 2, tileId: 66 },
+        { x: 1, y: 2, tileId: 67 },
+        { x: 2, y: 2, tileId: 68 },
+    ],
+    large_orange_bookcase: [
+        { x: 0, y: 0, tileId: 69 },
+        { x: 1, y: 0, tileId: 70 },
+        { x: 2, y: 0, tileId: 71 },
+        { x: 0, y: 1, tileId: 72 },
+        { x: 1, y: 1, tileId: 73 },
+        { x: 2, y: 1, tileId: 74 },
+        { x: 0, y: 2, tileId: 75 },
+        { x: 1, y: 2, tileId: 76 },
+        { x: 2, y: 2, tileId: 77 },
+    ],
+    lamp: [
+        { x: 0, y: 0, tileId: 78 },
+        { x: 0, y: 1, tileId: 79 },
+        { x: 0, y: 2, tileId: 80 },
+    ],
+    tan_hatstand: [
+        { x: 0, y: 0, tileId: 81 },
+        { x: 0, y: 1, tileId: 82 },
+        { x: 0, y: 2, tileId: 83 },
+    ],
+    orange_clock: [
+        { x: 0, y: 0, tileId: 84 },
+        { x: 0, y: 1, tileId: 85 },
+        { x: 0, y: 2, tileId: 86 },
+        { x: 1, y: 0, tileId: 87 },
+        { x: 1, y: 1, tileId: 88 },
+        { x: 1, y: 2, tileId: 89 }
+    ],
+    orange_couch: [
+        { x: 0, y: 0, tileId: 90 },
+        { x: 1, y: 0, tileId: 91 },
+        { x: 2, y: 0, tileId: 92 },
+        { x: 3, y: 0, tileId: 93 },
+        { x: 0, y: 1, tileId: 94 },
+        { x: 1, y: 1, tileId: 95 },
+        { x: 2, y: 1, tileId: 96 },
+        { x: 3, y: 1, tileId: 97 }
+    ],
 };
 
 class Block {
